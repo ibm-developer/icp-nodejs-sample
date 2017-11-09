b5c3ae3266f93cda503252a06b102ea9faf17914
Adam Roberts <aroberts@uk.ibm.com>
2017-11-09 14:23:12 +0000

    Fix indenting inherited from our starter projects

--- server/routers/public.js
+++ server/routers/public.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 module.exports = function(app){
-	var router = express.Router();
-	router.use(express.static(process.cwd() + '/public'));
-	app.use(router);
+  var router = express.Router();
+  router.use(express.static(process.cwd() + '/public'));
+  app.use(router);
 }
