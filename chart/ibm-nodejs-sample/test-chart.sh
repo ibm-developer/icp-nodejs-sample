3c46145ac7c3c4d769a30c49ff840e437e57c198
Adam Roberts <aroberts@uk.ibm.com>
2017-11-23 11:00:58 +0000

    Add a variety of tests, use dev image for now

--- chart/ibm-nodejs-sample/test-chart.sh
+++ chart/ibm-nodejs-sample/test-chart.sh
@@ -1,4 +1,13 @@
-helm delete --purge sample
-kubectl delete pod sample-wget-test 2> /dev/null
+helm delete --purge sample 2> /dev/null
+kubectl delete pod sample-main-endpoint-test 2> /dev/null
+kubectl delete pod sample-metrics-endpoint-test 2> /dev/null
+kubectl delete pod sample-dash-endpoint-test 2> /dev/null
+kubectl delete pod sample-stays-up-test 2> /dev/null
+
+sleep 10
+
 helm install --name sample .
-helm test --debug --timeout 120 sample
+
+sleep 10
+
+helm test --cleanup --debug --timeout 30 sample
