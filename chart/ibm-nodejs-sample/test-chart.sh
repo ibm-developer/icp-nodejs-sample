0588fafa1f80b6129de838424b47a7b8296916c9
Adam Roberts <aroberts@uk.ibm.com>
2017-12-06 15:42:48 +0000

    Fix a comment

--- chart/ibm-nodejs-sample/test-chart.sh
+++ chart/ibm-nodejs-sample/test-chart.sh
@@ -4,10 +4,10 @@ kubectl delete pod sample-metrics-endpoint-test 2> /dev/null
 kubectl delete pod sample-dash-endpoint-test 2> /dev/null
 kubectl delete pod sample-stays-up-test 2> /dev/null
 
-sleep 10
+sleep 15
 
 helm install --name sample .
 
-sleep 30
+sleep 45
 
 helm test --cleanup --debug --timeout 45 sample
